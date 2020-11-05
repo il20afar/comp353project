85542e6f7f7c243100b53e69276c242052232708
Rohhaan Thambithurai <rohhaan.t@gmail.com>
2020-11-05 17:01:38 -0500

    switched to new branch with updated reviews and ads

--- frontend/con_manager/src/AppContainer/PageContainer/Pages/Social/Reviews.js
+++ frontend/con_manager/src/AppContainer/PageContainer/Pages/Social/Reviews.js
@@ -3,9 +3,45 @@ import { D } from "../../../../imports";
 import "./Reviews.scss";
 
 const Reviews = (props) => {
-  const {} = props;
+  const { userReview } = props;
+  const [state, setState] = React.useState();
 
-  return <D cn="Reviews"></D>;
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = state;
+    console.log("Final data is", data);
+  };
+
+  const handleInputChange = (event) => {
+    event.preventDefault();
+    setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  return (
+    <div className="Reviews">
+      <h1>User Reviews</h1>
+      <p>Review: {userReview} </p>
+
+      <form onSubmit={handleSubmit}>
+        <p>
+          <input
+            type="text"
+            placeholder="Users Review"
+            value={userReview}
+            name="userReview"
+            onChange={handleInputChange}
+          ></input>
+        </p>
+        <p>
+          <button>Send Message</button>
+        </p>
+      </form>
+    </div>
+  );
+
+  //return <D cn="Reviews"></D>;
 };
 
 export default Reviews;
