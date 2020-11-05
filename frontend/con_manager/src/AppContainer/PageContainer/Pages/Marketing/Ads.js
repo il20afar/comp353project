79c5fa908c0f23873a266d97747ab649e91174d9
Rohhaan Thambithurai <rohhaan.t@gmail.com>
2020-11-05 17:11:23 -0500

    updated ads again

--- frontend/con_manager/src/AppContainer/PageContainer/Pages/Marketing/Ads.js
+++ frontend/con_manager/src/AppContainer/PageContainer/Pages/Marketing/Ads.js
@@ -1,43 +1,53 @@
-import React from 'react';
-import Condo from './Condo';
-import './Ads.scss';
+import React from "react";
+import { D } from "../../../../imports";
+import "./Ads.scss";
 
-    const Ads = (props) => {
+const Ads = (props) => {
+  const { advertisements } = props;
+  const arr = [
+    {
+      adId: 1,
+      img:
+        "https://www.brickunderground.com/sites/default/files/styles/blog_primary_image/public/blog/images/190501Tribeca111MurrayStMAINPIC.jpg",
+      city: "Montreal",
+      price: "300003434",
+    },
+    {
+      adId: 4,
+      img:
+        "https://www.brickunderground.com/sites/default/files/styles/blog_primary_image/public/blog/images/190501Tribeca111MurrayStMAINPIC.jpg",
+      city: "Montreal",
+      price: "100023432400",
+    },
+    {
+      adId: 7,
+      img:
+        "https://www.brickunderground.com/sites/default/files/styles/blog_primary_image/public/blog/images/190501Tribeca111MurrayStMAINPIC.jpg",
+      city: "Montreal",
+      price: "13334200000",
+    },
+    {
+      adId: 67,
+      img:
+        "https://www.brickunderground.com/sites/default/files/styles/blog_primary_image/public/blog/images/190501Tribeca111MurrayStMAINPIC.jpg",
+      city: "Ottawa",
+      price: "123400000",
+    },
+  ];
+  console.log(JSON.stringify(arr));
 
+  const makeCondo = (adId, img, city, price) => {
+    return <Condo {...{ className: "condoAd", adId, img, city, price }} />;
+  };
 
-
-        return(
-        <div className = "ads">
-        <header className = "ads-head">Discover Your Future Home</header>
-        <div className = "container">
-        <Condo className = "container-element"
-            imgURL = "https://www.brickunderground.com/sites/default/files/styles/blog_primary_image/public/blog/images/190501Tribeca111MurrayStMAINPIC.jpg" 
-            city = "Monstreal"
-            price = "$120000"
-            />
-
-        <Condo className = "container-element"
-            imgURL = "https://www.brickunderground.com/sites/default/files/styles/blog_primary_image/public/blog/images/190501Tribeca111MurrayStMAINPIC.jpg" 
-            city = "Monstreal"
-            price = "$120000"
-            />
-
-        <Condo className = "container-element"
-            imgURL = "https://www.brickunderground.com/sites/default/files/styles/blog_primary_image/public/blog/images/190501Tribeca111MurrayStMAINPIC.jpg" 
-            city = "Monstreal"
-            price = "$120000"
-            />
-
-<Condo className = "container-element"
-            imgURL = "https://www.brickunderground.com/sites/default/files/styles/blog_primary_image/public/blog/images/190501Tribeca111MurrayStMAINPIC.jpg" 
-            city = "Monstreal"
-            price = "$120000"
-            />
-            </div>
-
-       
-        </div>
-        )
-    }
+  return (
+    <div className="ads">
+      <header className="ads-head">Discover Your Future Home</header>
+      <div className="contain">
+        {arr.map((obj) => makeCondo(obj.adId, obj.img, obj.city, obj.price))}
+      </div>
+    </div>
+  );
+};
 
 export default Ads;
