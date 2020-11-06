5dbb211aff0c425c8b93e91f4bdcbc08a362cada
Rohhaan Thambithurai <rohhaan.t@gmail.com>
2020-11-05 22:10:07 -0500

    version 1 of adDetails

--- frontend/con_manager/src/AppContainer/PageContainer/Pages/Marketing/AdDetail.js
+++ frontend/con_manager/src/AppContainer/PageContainer/Pages/Marketing/AdDetail.js
@@ -2,20 +2,26 @@ import React from "react";
 import "./AdDetail.scss";
 
 const AdDetail = (props) => {
-    return (
-        <div className = "ad-detail">
-            <img className = "img-detail" src= {props.img}/>
-            <div className = "title-id-box">
-            <p className = "title">{props.title}</p>
-            <p className = "id">Adverstment ID: {props.adId}</p>
-            </div>
-            <p>Type: {props.type}</p>
-            <p>City: {props.city}</p>
-            <p>Price: {props.price}</p>
-        </div>
-    )
+  return (
+    <div>
+      <header>
+        <h2>{props.title}</h2>
+      </header>
 
+      <section>
+        <nav>
+          <img className="img-detail" src={props.img} />
+        </nav>
 
-}
+        <description>
+          <h1>Adverstment ID: {props.adId}</h1>
+          <p>Type: {props.type}</p>
+          <p>City: {props.city}</p>
+          <p>Price: {props.price}</p>
+        </description>
+      </section>
+    </div>
+  );
+};
 
 export default AdDetail;
--- frontend/con_manager/src/AppContainer/PageContainer/Pages/Marketing/Condo.js
+++ frontend/con_manager/src/AppContainer/PageContainer/Pages/Marketing/Condo.js
@@ -1,28 +1,37 @@
-import React from 'react';
-import './Condo.scss';
-import { BrowserRouter, Route, Link, NavLink, Switch, Redirect} from "react-router-dom";
+import React from "react";
+import "./Condo.scss";
+import {
+  BrowserRouter,
+  Route,
+  Link,
+  NavLink,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 
-
-    const Condo = (props) => {
-
-        return(
-        <BrowserRouter>
-        
-        <NavLink activeClassName = "active" to={"/"}><div className = "condo" >
-        <button className = "box" >
-        <img className = "img" src= {props.img}/>
-        <div className = "content">
-        <p>{props.title} </p>
-        <p>{props.price} </p>
-        
+const Condo = (props) => {
+  return (
+    <BrowserRouter>
+      <NavLink activeClassName="active" to={"/"}>
+        <div className="condo">
+          <button className="box">
+            <img className="img" src={props.img} />
+            <div className="content">
+              <p>{props.title} </p>
+              <p>{props.price} </p>
+            </div>
+          </button>
         </div>
       </NavLink>
 
-        <Switch>
-        <Route path='/faq' component={() => window.location.pathname = '/detail'}/>
-        </Switch>
-        </BrowserRouter>
-        )
-    }
+      <Switch>
+        <Route
+          path="/faq"
+          component={() => (window.location.pathname = "/detail")}
+        />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default Condo;
