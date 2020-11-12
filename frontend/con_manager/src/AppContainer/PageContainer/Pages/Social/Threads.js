87c7c7c9547b02d528ce2362f6bf5c51f9c4be27
il20afar <antoinefarley@gmail.com>
2020-11-11 20:43:13 -0500

    (a1_frontend_antoine): additional changes

---
+++ frontend/con_manager/src/AppContainer/PageContainer/Pages/Social/Threads.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { D } from "../../../../imports";
+import "./Threads.scss";
+
+const Threads = (props) => {
+  const {} = props;
+
+  return <D cn="Threads"></D>;
+};
+
+export default Threads;
