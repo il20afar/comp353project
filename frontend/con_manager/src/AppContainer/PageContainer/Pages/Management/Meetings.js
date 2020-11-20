12b71a48f891afafab58554651caf7347301f3a8
Rohhaan Thambithurai <rohhaan.t@gmail.com>
2020-11-20 17:02:35 -0500

    started adding financial components, working funcitonality, weird styling

--- frontend/con_manager/src/AppContainer/PageContainer/Pages/Management/Meetings.js
+++ frontend/con_manager/src/AppContainer/PageContainer/Pages/Management/Meetings.js
@@ -2,12 +2,6 @@ import React from "react";
 import "./Meetings.scss";
 import Agenda from "../../../../Components/Agenda/Agenda";
 
-// const Meetings = (props) => {
-//   const {} = props;
-
-//   return <D cn="Meetings"></D>;
-// };
-
 class Meetings extends React.Component {
   constructor(props) {
     super(props);
---
+++ frontend/con_manager/src/Components/ExpenseTracker/Header.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export const Header = () => {
+  return <h4>Expense Tracker</h4>;
+};
+export default Header;
