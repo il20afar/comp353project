217d34f5afe10221af1107a601f5150e0568a4fd
Maxim Pobudzey <maxim.pobudzey@net4user.com>
2020-12-04 16:20:49 -0500

    Fixed attend endpoint

--- backend/activities.php
+++ backend/activities.php
@@ -14,13 +14,28 @@ class activities extends request
         return json_encode($res);
     }
 
+    public function get($obj)
+    {
+        $asso_id = $obj['asso_id'];
+    }
+
     public function attend($obj)
     {
         $user_id = $obj['user_id'];
         $activity_id = $obj['activity_id'];
+        // Add (user_id, activity_id) pair to attends table
         $query = sprintf(
             'INSERT INTO attends (user_id, activity_id) VALUES (%s, %s);',
             $user_id,
+            $activity_id
+        );
+        $res = $this->query($query, false);
+        if ($res != 1) {
+            return json_encode(-1);
+        }
+        // Increment number_of_attendees of given activity
+        $query = sprintf(
+            'UPDATE activities SET number_of_attendees = number_of_attendees + 1 WHERE activity_id=%s;',
             $activity_id
         );
         $res = $this->query($query, false);
