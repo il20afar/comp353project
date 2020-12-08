fdc269a3e3c6794d2038a6a67d850fc76d800226
il20afar <antoinefarley@gmail.com>
2020-12-07 23:32:35 -0500

    afsdfasdfadsf

--- backend/main.php
+++ backend/main.php
@@ -2,13 +2,6 @@
 include 'db_config.php';
 include 'router.php';
 
-$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-$txt = "John Doe\n";
-fwrite($myfile, $txt);
-$txt = "Jane Doe\n";
-fwrite($myfile, $txt);
-fclose($myfile);
-
 $conn = mysqli_connect($hostname, $db_username, $db_password, $database_name);
 $router = new router($conn);
 $router->route();
